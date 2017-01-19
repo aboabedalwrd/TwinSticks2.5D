a29bdb4372b531b51c9ef918a08554ba8e8ebcbe
AbdAlrahman Loulou <aboabed14@gmail.com>
2017-01-19 06:06:28 +0200

    Refactored the Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,40 +5,17 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class CameraController : MonoBehaviour {
 
-	private float h;
-	private float v;
-	private Vector3 cameraRotation;
 	private GameObject player;
-	private Vector3 playerPosition;
-	private Vector3  cameraOffset;
 
 
 	void Start () {
 		player = GameObject.FindGameObjectWithTag("Player");
-		cameraRotation = transform.rotation.eulerAngles;
-		cameraOffset = transform.position-player.transform.position;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		 h = CrossPlatformInputManager.GetAxis("CHorizontal");
-    	 v = CrossPlatformInputManager.GetAxis("CVertical");
-    	 cameraRotation.y += h ;
-    	 cameraRotation.x -= v ;
-		RotateCamera(cameraRotation);
-
-		FollowPlayer();
+	void LateUpdate () {
+		transform.LookAt(player.transform);
 	}
-
-	private void RotateCamera(Vector3 rotateDirection){
-		transform.rotation = Quaternion.Euler(rotateDirection);
-	}
-
-	private void FollowPlayer(){
-		transform.position = player.transform.position + cameraOffset;
-	} 
-          
-
-
 
 }
---
+++ Assets/Scripts/SelfieStick.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class SelfieStick : MonoBehaviour {
+
+	private float h;
+	private float v;
+	private GameObject player;
+	private Vector3 armRotation;
+
+
+	void Start () {
+		player = GameObject.FindGameObjectWithTag("Player");
+		armRotation =transform.rotation.eulerAngles;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   	 h = CrossPlatformInputManager.GetAxis("CHorizontal");
+    	 v = CrossPlatformInputManager.GetAxis("CVertical");
+    	 armRotation.y -= h;
+		 armRotation.z += v;
+		 FollowPlayer();
+		RotateStick(armRotation);
+	}
+
+	private void FollowPlayer(){
+
+		transform.position = player.transform.position;
+	}
+
+	private void RotateStick(Vector3 rotateDirection){
+		transform.rotation = Quaternion.Euler(rotateDirection);
+	}
+
+}
